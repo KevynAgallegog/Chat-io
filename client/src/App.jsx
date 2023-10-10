c5f9a686ff029d0dedd1e3f27959f36c506b5842
Kevyn gallego <gallegokevyn10@gmail.com>
2023-10-10 08:38:34 -0500

    chat

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+import {chatClient} from './components/chatClient/chatClient';
+
+function App() {
+ 
+
+  return (
+    <div className='App'>
+      <chatClient/>
+    </div>
+  )
+}
+
+export default App
